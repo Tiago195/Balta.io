fb2de0632b356ecff76a0360add67ff02f82ad14
Tiago195 <tiagocf475@gmail.com>
2023-01-24 11:30:24 -0300

    feat: :sparkles: Soma

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,9 +7,14 @@ public class Program
   static void Main(string[] args)
   {
     Console.Clear();
-    Console.WriteLine("Primeiro valor:");
+    Console.Write("Primeiro valor: ");
     double v1 = double.Parse(Console.ReadLine());
 
-    System.Console.WriteLine(v1);
+    Console.Write("Segundo valor: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    System.Console.WriteLine("");
+
+    Console.WriteLine($"Resultado da soma é {v1 + v2}");
   }
 }
