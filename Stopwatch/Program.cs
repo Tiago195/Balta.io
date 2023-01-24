b39a0cad787ea2591d761bdac1bafd37aaaf4e50
Tiago195 <tiagocf475@gmail.com>
2023-01-24 13:42:12 -0300

    chore: :construction: Start stopwatch

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -6,6 +6,24 @@ public class Program
 {
   static void Main(string[] args)
   {
+    Start();
+  }
 
+  static void Start()
+  {
+    int time = 10;
+
+    int currentTime = 0;
+
+    while (currentTime != time)
+    {
+      System.Console.Clear();
+
+      currentTime++;
+
+      System.Console.WriteLine(currentTime);
+
+      Thread.Sleep(1000);
+    }
   }
 }
