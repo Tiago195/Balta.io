5fe68cee406a34f983a698a425f35854810c7add
Tiago195 <tiagocf475@gmail.com>
2023-01-24 11:38:01 -0300

    feat: :sparkles: Subtração

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -23,4 +23,18 @@ public class Program
     Console.WriteLine($"Resultado da soma é {v1 + v2}");
     Console.ReadKey();
   }
+  static void Subtracao()
+  {
+    Console.Clear();
+    Console.Write("Primeiro valor: ");
+    double v1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Segundo valor: ");
+    double v2 = double.Parse(Console.ReadLine());
+
+    System.Console.WriteLine("");
+
+    Console.WriteLine($"Resultado da subtração é {v1 - v2}");
+    Console.ReadKey();
+  }
 }
