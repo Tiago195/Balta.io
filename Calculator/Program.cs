336d3a673be1e8dd1c321b37e569da3931c24982
Tiago195 <tiagocf475@gmail.com>
2023-01-24 11:21:15 -0300

    feat: :tada:

---
+++ Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Clear();
+    Console.WriteLine("Primeiro valor:");
+    double v1 = double.Parse(Console.ReadLine());
+
+    System.Console.WriteLine(v1);
+  }
+}
