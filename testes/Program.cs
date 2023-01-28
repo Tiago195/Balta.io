f4b260a6d6e1262cdfa3d02624bbf0d96f81eb78
Tiago195 <tiagocf475@gmail.com>
2023-01-28 12:01:27 -0300

    feat: :tada: testes

---
+++ testes/Program.cs
@@ -0,0 +1,31 @@
+﻿// System.Console.Clear();
+// System.Console.WriteLine(DateTime.Now.ToString("D"));
+// System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
+// System.Console.WriteLine(DateTime.Now.ToString("D"));
+// System.Console.WriteLine(10.25m.ToString("C"));
+// System.Console.WriteLine(Math.Round(15.4));
+
+// public class Program
+// {
+//   static void Main(string[] args)
+//   {
+//     var teste = new Food<FastFood>();
+
+//     teste.Eat(new FastFood());
+//   }
+
+
+//   public class Food<T>
+//   where T : FastFood
+//   {
+//     public void Eat(T food)
+//     {
+//       System.Console.WriteLine("Food is thrash: " + food.Calorias);
+//     }
+//   }
+
+//   public class FastFood
+//   {
+//     public int Calorias { get; set; }
+//   }
+// }
