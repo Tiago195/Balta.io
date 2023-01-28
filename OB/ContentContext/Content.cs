cd0a5214ad3cd424c7f9468749f78fb49589a21e
Tiago195 <tiagocf475@gmail.com>
2023-01-28 12:12:50 -0300

    chore: :construction: change type Id

--- OB/ContentContext/Content.cs
+++ OB/ContentContext/Content.cs
@@ -2,7 +2,12 @@ namespace OB.ContentContext;
 
 public class Content
 {
-  public int Id { get; set; }
+  public Guid Id { get; set; }
   public string Title { get; set; }
   public string Url { get; set; }
+
+  public Content()
+  {
+    Id = Guid.NewGuid();
+  }
 }
