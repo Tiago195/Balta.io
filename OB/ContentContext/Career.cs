653498406a0f1b699f7deada97cc475c1d37fb6a
Tiago195 <tiagocf475@gmail.com>
2023-01-28 12:43:18 -0300

    feat: :sparkles: Implements Carrer

--- OB/ContentContext/Career.cs
+++ OB/ContentContext/Career.cs
@@ -2,5 +2,11 @@ namespace OB.ContentContext;
 
 public class Career : Content
 {
-
+  public IList<CareerItem> Items { get; set; }
+  // Expression body
+  public int TotalCourses => Items.Count;
+  public Career()
+  {
+    Items = new List<CareerItem>();
+  }
 }
