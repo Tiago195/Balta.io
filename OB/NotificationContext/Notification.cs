ea646c607e0ae9788b61a09ab222f1a98b6eb6ed
Tiago195 <tiagocf475@gmail.com>
2023-01-28 13:38:54 -0300

    feat: :sparkles: class Notification

---
+++ OB/NotificationContext/Notification.cs
@@ -0,0 +1,16 @@
+namespace OB.NotificationContext;
+
+public sealed class Notification
+{
+  public string Property { get; set; }
+  public string Message { get; set; }
+
+  public Notification() { }
+
+  public Notification(string property, string message)
+  {
+    Property = property;
+    Message = message;
+  }
+
+}
